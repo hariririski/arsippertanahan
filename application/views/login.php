39aff748cdba8e2ef2b2409263766e539c5af320
Hariri Riski <hariririski@gmail.com>
2021-03-23 11:17:15 +0700

    update

--- application/views/login.php
+++ application/views/login.php
@@ -35,13 +35,14 @@
                 </div>
                 <!-- END: Login Info -->
                 <!-- BEGIN: Login Form -->
+
               <form class="login-form" method="post" action="<?php echo base_url(); ?>Login/proses_login">
                 <div class="h-screen xl:h-auto flex py-5 xl:py-0 my-10 xl:my-0">
                     <div class="my-auto mx-auto xl:ml-20 bg-white dark:bg-dark-1 xl:bg-transparent px-5 sm:px-8 py-8 xl:p-0 rounded-md shadow-md xl:shadow-none w-full sm:w-3/4 lg:w-2/4 xl:w-auto">
                         <h2 class="intro-x font-bold text-2xl xl:text-3xl text-center xl:text-left">
-                            Login
+                            Masuk
                         </h2>
-                        <div class="intro-x mt-2 text-gray-500 xl:hidden text-center">A few more clicks to sign in to your account. Manage all your e-commerce accounts in one place</div>
+                        <div class="intro-x mt-2 text-gray-500 xl:hidden text-center">Kelola Arsip Pertanahan Di Satu Aplikasi Serta Temukan Arsip Pertanahan Lebih Mudah dan Cepat</div>
                         <div class="intro-x mt-8">
                             <input type="text" class="intro-x login__input form-control py-3 px-4 border-gray-300 block" placeholder="User Name" name="username">
                             <input type="password" class="intro-x login__input form-control py-3 px-4 border-gray-300 block mt-4" placeholder="Password" name="password">
@@ -49,17 +50,26 @@
 
                         <div class="intro-x mt-5 xl:mt-8 text-center xl:text-left">
                             <button class="btn btn-primary py-3 px-4 w-full xl:w-32 xl:mr-3 align-top">Login</button>
-                            <button class="btn btn-outline-secondary py-3 px-4 w-full xl:w-32 mt-3 xl:mt-0 align-top">Sign up</button>
+
                         </div>
-                        <div class="intro-x mt-10 xl:mt-24 text-gray-700 dark:text-gray-600 text-center xl:text-left">
-                            By signin up, you agree to our
-                            <br>
-                            <a class="text-theme-17 dark:text-gray-300" href="">Terms and Conditions</a> & <a class="text-theme-17 dark:text-gray-300" href="">Privacy Policy</a>
+                        <br>
+                        <br>
+                        <br>
+                        <br>
+                        <br>
+                        <div class="hidden xl:flex flex-col">
+                            <img alt="Icewall Tailwind HTML Admin Template" width="200%" src="dist/images/kantor.png">
+
                         </div>
+
+
                     </div>
+
                 </div>
               </form>
+
                 <!-- END: Login Form -->
+
             </div>
         </div>
 
