00840c7a5b60f48aebe8582ce627fe61ac41bbfc
Hariri Riski <hariririski@gmail.com>
2021-06-18 17:20:55 +0700

    Update scan2.php

--- application/views/scan2.php
+++ application/views/scan2.php
@@ -78,7 +78,7 @@
                     lastResult = decodedText;
                     // Handle on success condition with the decoded message.
                     console.log(`Scan result ${decodedText}`, decodedResult);
-                    alert(decodedResult);
+                    alert(lastResult);
                 }
             }
 
