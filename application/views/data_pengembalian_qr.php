08e33215420b163f57a4f9233981e6035790eba4
Hariri Riski <hariririski@gmail.com>
2021-06-20 21:00:45 +0700

    upsate

--- application/views/data_pengembalian_qr.php
+++ application/views/data_pengembalian_qr.php
@@ -200,7 +200,7 @@
 
 
         function tabel(barcode){
-          alert("<?php echo base_url('pinjam/cari_barcode_pengembalian')?>/<?php echo $this->session->userdata("nama_lengkap"); ?>/"+barcode);
+          
           $.ajax({
               type : "POST",
               url  : "<?php echo base_url('pinjam/cari_barcode_pengembalian')?>/<?php echo $this->session->userdata("nama_lengkap"); ?>/"+barcode,
