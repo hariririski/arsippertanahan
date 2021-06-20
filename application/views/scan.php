4c0cf4612a18184697679d55284d040f6248c0ff
Hariri Riski <hariririski@gmail.com>
2021-06-20 22:32:43 +0700

    Update scan.php

--- application/views/scan.php
+++ application/views/scan.php
@@ -83,7 +83,7 @@
             }
 
             var html5QrcodeScanner = new Html5QrcodeScanner(
-                "qr-reader", { fps: 10, qrbox: 250 });
+                "qr-reader", { fps: 2, qrbox: 250 });
             html5QrcodeScanner.render(onScanSuccess);
         });
     </script>
