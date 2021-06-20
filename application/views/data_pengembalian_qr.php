bddaf33659122f9a365c002f4de36bfc8cfdbb37
Hariri Riski <hariririski@gmail.com>
2021-06-20 20:01:08 +0700

    Update data_pengembalian_qr.php

--- application/views/data_pengembalian_qr.php
+++ application/views/data_pengembalian_qr.php
@@ -151,7 +151,7 @@
             }
 
             var html5QrcodeScanner = new Html5QrcodeScanner(
-                "qr-reader", { fps: 2, qrbox: 180 });
+                "qr-reader", { fps: 2, qrbox: 250 });
             html5QrcodeScanner.render(onScanSuccess);
         });
     </script>
