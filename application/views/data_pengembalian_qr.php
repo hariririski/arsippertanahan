534cbafc6768173d74a571c2fc3fe4255f7f2735
Hariri Riski <hariririski@gmail.com>
2021-06-20 20:48:59 +0700

    Update data_pengembalian_qr.php

--- application/views/data_pengembalian_qr.php
+++ application/views/data_pengembalian_qr.php
@@ -162,36 +162,36 @@
           if(data!=null){
             pecah=data.split(";",10);
             simpan();
-            if(pecah[8]=="BT"){
-              pecah[4]=pecah[4]+";"+pecah[9];
-              $('[name="id_pinjam"]').val(pecah[4]);
-              pecah[0]=pecah[1]+" / "+pecah[7]+" / "+pecah[0];
-              $('#id').html(pecah[0]);
-              $('#tgl_pinjam').html(pecah[2]);
-              $('#tgl_kembali').html(pecah[3]);
-              $('#nama_lengkap').html(pecah[5]);
-              $('#selisih').html(pecah[6]);
-            }else if(pecah[8]=="SU"){
-              pecah[4]=pecah[4]+";"+pecah[9];
-              $('[name="id_pinjam"]').val(pecah[4]);
-              pecah[0]=pecah[1]+" / "+pecah[0]+" / "+pecah[7];
-              $('#id').html(pecah[0]);
-              $('#desa').html(pecah[1]);
-              $('#tgl_pinjam').html(pecah[2]);
-              $('#tgl_kembali').html(pecah[3]);
-              $('#nama_lengkap').html(pecah[5]);
-              $('#selisih').html(pecah[6]);
-            }else if(pecah[8]=="W"){
-              pecah[4]=pecah[4]+";"+pecah[9];
-              $('[name="id_pinjam"]').val(pecah[4]);
-              pecah[0]=pecah[0]+" / "+pecah[7];
-              $('#id').html(pecah[0]);
-              $('#desa').html(pecah[1]);
-              $('#tgl_pinjam').html(pecah[2]);
-              $('#tgl_kembali').html(pecah[3]);
-              $('#nama_lengkap').html(pecah[5]);
-              $('#selisih').html(pecah[6]);
-            }
+            // if(pecah[8]=="BT"){
+            //   pecah[4]=pecah[4]+";"+pecah[9];
+            //   $('[name="id_pinjam"]').val(pecah[4]);
+            //   pecah[0]=pecah[1]+" / "+pecah[7]+" / "+pecah[0];
+            //   $('#id').html(pecah[0]);
+            //   $('#tgl_pinjam').html(pecah[2]);
+            //   $('#tgl_kembali').html(pecah[3]);
+            //   $('#nama_lengkap').html(pecah[5]);
+            //   $('#selisih').html(pecah[6]);
+            // }else if(pecah[8]=="SU"){
+            //   pecah[4]=pecah[4]+";"+pecah[9];
+            //   $('[name="id_pinjam"]').val(pecah[4]);
+            //   pecah[0]=pecah[1]+" / "+pecah[0]+" / "+pecah[7];
+            //   $('#id').html(pecah[0]);
+            //   $('#desa').html(pecah[1]);
+            //   $('#tgl_pinjam').html(pecah[2]);
+            //   $('#tgl_kembali').html(pecah[3]);
+            //   $('#nama_lengkap').html(pecah[5]);
+            //   $('#selisih').html(pecah[6]);
+            // }else if(pecah[8]=="W"){
+            //   pecah[4]=pecah[4]+";"+pecah[9];
+            //   $('[name="id_pinjam"]').val(pecah[4]);
+            //   pecah[0]=pecah[0]+" / "+pecah[7];
+            //   $('#id').html(pecah[0]);
+            //   $('#desa').html(pecah[1]);
+            //   $('#tgl_pinjam').html(pecah[2]);
+            //   $('#tgl_kembali').html(pecah[3]);
+            //   $('#nama_lengkap').html(pecah[5]);
+            //   $('#selisih').html(pecah[6]);
+            // }
           }else{
             peringatan("Arsip Tidak Sedang Di Pinjam");
           }
@@ -354,9 +354,7 @@
                     // Handle on success condition with the decoded message.
                     console.log(`Scan result ${decodedText}`, decodedResult);
                     audio.play();
-
-                }else{
-                  tabel("SU-04TVZvDGNaORx7PJfeUy");
+                    tabel("SU-04TVZvDGNaORx7PJfeUy");
                 }
             }
 
