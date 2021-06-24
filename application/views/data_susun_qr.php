b18dacb9a4d86812a0e5b40e84388dc3f6404e07
Hariri Riski <hariririski@gmail.com>
2021-06-24 20:28:59 +0700

    Update data_susun_qr.php

--- application/views/data_susun_qr.php
+++ application/views/data_susun_qr.php
@@ -157,6 +157,7 @@
           if(data!=null){
             pecah=data.split(";",10);
             //simpan();
+            alert("masuk");
             if(pecah[8]=="BT" || pecah[8]=="SU" || pecah[8]=="W"){
                 window.location.href='<?php echo base_url();?>'+"pinjam/detail_susunqr/"+pecah[4];
             }else{
