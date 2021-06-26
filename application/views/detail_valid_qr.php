06ce237052120547da3785f120c56361a79750ad
Hariri Riski <hariririski@gmail.com>
2021-06-26 20:57:58 +0700

    Update detail_valid_qr.php

--- application/views/detail_valid_qr.php
+++ application/views/detail_valid_qr.php
@@ -84,7 +84,7 @@
                               </select>
                             </div>
                       </div>
-                      <?php if($value->valid==1){echo'<img src="'.base_url().'assets/images/valid.png">';} ?>
+                      <?php if($value->valid==1){echo'<img src="'.base_url().'assets/images/valid.png" width="40%">';} ?>
                           <input name="id" id="id" type="hidden" value="<?php echo $type."-".$id; ?>">
 
                     </div>
