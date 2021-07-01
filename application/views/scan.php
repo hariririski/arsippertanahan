2583fad9b447a1a266a76a611f1fc568365108ac
Hariri Riski <hariririski@gmail.com>
2021-07-01 22:41:53 +0700

    update

--- application/views/scan.php
+++ application/views/scan.php
@@ -41,39 +41,11 @@
                     <div class="p-b-40 p-t-20">
                         <h3 class="white-text">Selamat Datang <?php echo $this->session->userdata("nama_lengkap"); ?>!</h3>
                         <p class="white-text op-7 m-b-20">Manajemen Arsip Lebih Mudah, Cepat, Mudah dan Efektif</p>
+                        <video id="preview" style="width:100%"></video>
                     </div>
                 </div>
             </div>
-            <!-- ============================================================== -->
-            <!-- Container fluid scss in scafholding.scss -->
-            <!-- ============================================================== -->
-            <div class="container-fluid">
-                <!-- ============================================================== -->
-                <!-- Devices - Income - Sales -->
-                <!-- ============================================================== -->
-                <div class="row">
-                    <!-- col -->
-                    <div class="col s12 ">
-                        <div class="card card-hover">
-                            <div class="card-content">
-                              <h4 class="card-title">Peminjaman</h4>
-                              <div>
-                                <video id="preview"></video>
-                              </div>
 
-
-                            </div>
-                        </div>
-                    </div>
-                    <!-- col -->
-
-                </div>
-
-
-            </div>
-            <!-- ============================================================== -->
-            <!-- Container fluid scss in scafholding.scss -->
-            <!-- ============================================================== -->
           <?php echo $this->load->view('share/footer', '', TRUE);?>
         </div>
 
@@ -81,8 +53,8 @@
     <!-- ============================================================== -->
     <!-- All Required js -->
     <!-- ============================================================== -->
-    <script src="https://cdnjs.cloudflare.com/ajax/libs/jquery/3.3.1/jquery.min.js"></script>
-    <script src="https://rawgit.com/schmich/instascan-builds/master/instascan.min.js"></script>
+    <script src="dist/js/jquery.min.js"></script>
+    <script src="dist/js/instascan.min.js"></script>
     <script type="text/javascript">
       let scanner = new Instascan.Scanner({ video: document.getElementById('preview') });
       scanner.addListener('scan', function (content) {
@@ -92,14 +64,14 @@
     	if (cameras.length > 0) {
     	  scanner.start(cameras[2]);
     	} else {
-    	  console.error('No cameras found.');
+    	  alert.error('No cameras found.');
     	}
       }).catch(function (e) {
-    	console.error(e);
+    	alert.error(e);
       });
     </script>
 
-    <script src="assets/libs/jquery/dist/jquery.min.js"></script>
+
     <script src="dist/js/materialize.min.js"></script>
     <script src="assets/libs/perfect-scrollbar/dist/js/perfect-scrollbar.jquery.min.js"></script>
     <!-- ============================================================== -->
@@ -115,18 +87,14 @@
     <!-- ============================================================== -->
     <!-- This page plugin js -->
     <!-- ============================================================== -->
-    <script src="assets/libs/chartist/dist/chartist.min.js"></script>
-    <script src="assets/libs/chartist-plugin-tooltips/dist/chartist-plugin-tooltip.min.js"></script>
+
     <script src="assets/libs/gaugeJS/dist/gauge.min.js"></script>
     <script src="assets/extra-libs/c3/d3.min.js"></script>
     <script src="assets/extra-libs/c3/c3.min.js"></script>
     <script src="assets/extra-libs/sparkline/sparkline.js"></script>
-    <script src="assets/libs/chart.js/dist/chart.min.js"></script>
-    <script src="dist/js/pages/chartjs/chartjs.init.js"></script>
-    <!-- <script src="dist/js/pages/dashboards/dashboard3.js"></script> -->
-    <script src="assets/extra-libs/jquery-datatables-editable/jquery.dataTables.js"></script>
-    <script src="assets/extra-libs/tiny-editable/mindmup-editabletable.js"></script>
-    <script src="assets/extra-libs/tiny-editable/numeric-input-example.js"></script>
+
+
+
 
 </body>
 
