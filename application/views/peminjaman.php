6e586afe341cfa62cc3b6628a5bd01923439736f
Hariri Riski <hariririski@gmail.com>
2021-07-04 00:39:22 +0700

    Update peminjaman.php

--- application/views/peminjaman.php
+++ application/views/peminjaman.php
@@ -629,7 +629,21 @@
                 });
 
 
-                
+  const formatTextWrap = (text, maxLineLength) => {
+  const words = text.replace(/[\r\n]+/g, ' ').split(' ');
+  let lineLength = 0;
+
+  // use functional reduce, instead of for loop
+  return words.reduce((result, word) => {
+    if (lineLength + word.length >= maxLineLength) {
+      lineLength = word.length;
+      return result + `\n${word}`; // don't add spaces upfront
+    } else {
+      lineLength += word.length + (result ? 1 : 0);
+      return result ? result + ` ${word}` : `${word}`; // add space only when needed
+    }
+  }, '');
+}
 
 
     </script>
