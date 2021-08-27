57e78bdb5c0325a22ed944beffafe26267a1f047
Hariri Riski <hariririski@gmail.com>
2021-08-27 20:14:56 +0700

    Update Login.php

--- application/controllers/Login.php
+++ application/controllers/Login.php
@@ -16,7 +16,7 @@ class Login extends CI_Controller {
 	{
 		$isMob = is_numeric(strpos(strtolower($_SERVER['HTTP_USER_AGENT']), "mobile"));
 		if($isMob){
-			redirect(base_url('logon/login_mobile'));
+			redirect(base_url('login/login_mobile'));
 		}else{
 			$this->load->view('login');
 		}
