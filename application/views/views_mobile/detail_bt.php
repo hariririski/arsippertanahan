3f79e5d0f21c371bb6e3e990a5f37abb8dc81598
Hariri Riski <hariririski@gmail.com>
2021-08-29 00:49:29 +0700

    Update detail_bt.php

--- application/views/views_mobile/detail_bt.php
+++ application/views/views_mobile/detail_bt.php
@@ -128,6 +128,9 @@
               </div>
             </div>
             <div class="divider"></div>
+            <a href="#" class="btn btn-full btn-l rounded-s font-800 text-uppercase gradient-highlight shadow-bg shadow-bg-m">Kembalikan/Susun Buku Tanah</a>
+            <a href="#" class="btn btn-full btn-l rounded-s font-800 text-uppercase gradient-highlight shadow-bg shadow-bg-m">Kembalikan/Susun Surat Ukur</a>
+            <a href="#" class="btn btn-full btn-l rounded-s font-800 text-uppercase gradient-highlight shadow-bg shadow-bg-m">Kambalikan/Susun Warkah</a>
             <a href="#" class="btn btn-full btn-l rounded-s font-800 text-uppercase gradient-highlight shadow-bg shadow-bg-m">Download Invoice in PDF</a>
           </div>
         </div>
