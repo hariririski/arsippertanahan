4a4d95da6f0f54fe1c6952795f05f158804ae998
Hariri Riski <hariririski@gmail.com>
2021-08-30 17:05:49 +0700

    update

--- application/views/views_mobile/share/header.php
+++ application/views/views_mobile/share/header.php
@@ -3,8 +3,8 @@
 <meta name="apple-mobile-web-app-status-bar-style" content="black-translucent">
 <meta name="viewport" content="width=device-width, initial-scale=1, minimum-scale=1, maximum-scale=1, viewport-fit=cover">
 <title>Mobile - Manajemen Arsip Pertanahan</title>
-<link rel="stylesheet" type="text/css" href="../assets_m/styles/bootstrap.css">
+<link rel="stylesheet" type="text/css" href="<?php echo base_url(); ?>assets_m/styles/bootstrap.css">
 <link rel="preconnect" href="https://fonts.gstatic.com">
-<link href="../assets_m/styles/css2.css" rel="stylesheet">
+<link href="<?php echo base_url(); ?>assets_m/styles/css2.css" rel="stylesheet">
 <meta id="theme-check" name="theme-color" content="#FFFFFF">
-<link rel="apple-touch-icon" sizes="180x180" href="../assets_m/app/icons/icon-192x192.png"></head>
+<link rel="apple-touch-icon" sizes="180x180" href="<?php echo base_url(); ?>assets_m/app/icons/icon-192x192.png"></head>
